4e76e988d7031a0fc6dd3b5a4053a128ad7b133b
sato <sera.martin@gmailcom>
2022-06-23 01:03:30 +0200

    pruebas qr

--- js/logica.js
+++ js/logica.js
@@ -1,17 +1,20 @@
 function muestraModal(contenido){
-    const modal = document.querySelector(".modal");
-    escondeHijas(clase)
-    const hijas = document.getElementsByClassName("modalhija");
-    contenido = document.getElementById(contenido);
-    for (hija of hijas) {
-        hija.style.display="none";
-    }
-    contenido.style.display="flex";
-    modal.style.display="flex";
+    const modal = document.querySelector(".modal"); // Selecciono la modal
+    contenido = document.getElementById(contenido);// Selecciono el contenido que debe verse en la modal
+    escondeHijas()    // Escondo todo lo que hay dentro de la modal
+    contenido.style.display="flex"; // Vuelvo mostrable el contenido que se ha escogido
+    modal.style.display="flex"; //Enseño la modal con el contenido correcto ya mostrable
 
 }
 
 function escondeModal(){
     const modal = document.querySelector(".modal");
     modal.style.display="none";
+}
+
+function escondeHijas(){
+    const hijas = document.getElementsByClassName("modalhija");
+    for (hija of hijas) {
+        hija.style.display="none";
+    }
 }
