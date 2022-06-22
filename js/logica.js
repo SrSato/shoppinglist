7a1c1ac260acec7497851ddc55342fb610e64ab3
srsato <sera.martin@gmail.com>
2022-06-22 12:21:12 +0200

    mejora de modales

--- js/logica.js
+++ js/logica.js
@@ -1,6 +1,14 @@
-function muestraModal(){
+function muestraModal(contenido){
     const modal = document.querySelector(".modal");
+    escondeHijas(clase)
+    const hijas = document.getElementsByClassName("modalhija");
+    contenido = document.getElementById(contenido);
+    for (hija of hijas) {
+        hija.style.display="none";
+    }
+    contenido.style.display="flex";
     modal.style.display="flex";
+
 }
 
 function escondeModal(){
