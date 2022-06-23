5aaf9fa552eb4defbcc1b06cc1a2fc8c5ce539a7
srsato <sera.martin@gmail.com>
2022-06-23 12:43:19 +0200

    creamos el infoprod

--- js/logica.js
+++ js/logica.js
@@ -1,8 +1,8 @@
-function muestraModal(contenido){
+function muestraModal(id_contenido){
     const modal = document.querySelector(".modal"); // Selecciono la modal
-    contenido = document.getElementById(contenido);// Selecciono el contenido que debe verse en la modal
+    id_contenido = document.getElementById(id_contenido);// Selecciono el contenido que debe verse en la modal
     escondeHijas()    // Escondo todo lo que hay dentro de la modal
-    contenido.style.display="flex"; // Vuelvo mostrable el contenido que se ha escogido
+    id_contenido.style.display="flex"; // Vuelvo mostrable el contenido que se ha escogido
     modal.style.display="flex"; //Enseño la modal con el contenido correcto ya mostrable
 
 }
