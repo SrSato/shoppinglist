d002712bdc7a9a519888873e597e372159afa31c
srsato <sera.martin@gmail.com>
2022-06-28 12:45:02 +0200

    empezamos con json

---
+++ js/tiendas.js
@@ -0,0 +1,20 @@
+let col_tiendas;
+
+col_tiendas = {
+    "tiendas" : [
+        {
+            "nombre":"Melones de Leyenda",
+            "direccion": "C/Mercadona nº 34 CP. 344343"
+        },
+        {
+            "nombre":"Mercadona",
+            "direccion": "C/Fruterias nº 26 CP. 434533"
+        },
+        {
+            "nombre":"ALDI",
+            "direccion":"C/ Del Rio nº 24 CP. 12600"
+        }
+    ]
+}
+
+
