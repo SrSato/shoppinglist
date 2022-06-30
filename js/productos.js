47d5006f2ba0fbdbc44fb90dfcfbc00fc218b11f
sato <sera.martin@gmailcom>
2022-07-01 01:04:03 +0200

    Una forma de listar por tiendas

--- js/productos.js
+++ js/productos.js
@@ -65,32 +65,46 @@ function limpiaProducto(){
 
 function rellenaLista(){
     /*
-    <table class="cebreado" id="tablaProds">
-        <tr>
-            <td> <input type="checkbox" name="chk_platano" id="chk_platano"></td>
-            <td> <label for="chk_platano">Plátano</label></td>
-            <td class="td_cant"> 12 </td>
-            <td class="td_icons"> <i class="material-icons" onclick="muestraModal('infoprod')">search</i><i class="material-icons" onclick='borraProducto("plátano")'>delete</i></td>
-        </tr>
-    </table>
+     <div class="tiendaProds" id="prod_">
+        <table class="cebreado" id="tablaProds">
+            <tr>
+                <td> <input type="checkbox" name="chk_platano" id="chk_platano"></td>
+                <td> <label for="chk_platano">Plátano</label></td>
+                <td class="td_cant"> 12 </td>
+                <td class="td_icons"> <i class="material-icons" onclick="muestraModal('infoprod')">search</i><i class="material-icons" onclick='borraProducto("plátano")'>delete</i></td>
+            </tr>
+        </table>
+    </div>
     */
+    document.getElementById("main").innerHTML = "";
 
-    let contenedor= document.getElementById("main");
-    let tabla = '<table class="cebreado" id="tablaProds">';
-
-    for (producto of col_productos.productos) {
-        filaTabla = 
+    for(tienda of col_tiendas.tiendas){
+        let prodTienda = col_productos.productos.filter( producto => producto.tienda == tienda.nombre);
+        
+        div = 
+        `<div class="tiendaProds invisible" id="${tienda.nombre.split(" ").join("")}Prods">
+            <h5> ${tienda.nombre}</h5>
+            <table class="cebreado" id="${tienda.nombre.split(" ").join("")}TablaProds">`
+        
+        for (producto of prodTienda){
+        div +=
             `<tr>
                 <td> <input type="checkbox" name="chk_${producto.nombre}" id="chk_${producto.nombre}"></td>
                 <td> <label for="chk_${producto.nombre}">${producto.nombre}</label></td>
                 <td class="td_cant"> ${producto.cantidad} </td>
                 <td class="td_icons"> <i class="material-icons" onclick="muestraModal('infoprod')">search</i><i class="material-icons" onclick='borraProducto("${producto.nombre}")'>delete</i></td>
             </tr>`;
-
-       tabla += filaTabla;        
-    }   
-    tabla +="</table>";
-    contenedor.innerHTML = tabla;
+        }
+        
+        div +=    
+        `   </table>
+        </div>`  
+        
+        document.getElementById("main").innerHTML += div;
+        if(prodTienda.length>0){
+            document.getElementById(`${producto.tienda.split(" ").join("")}Prods`).classList.remove("invisible");  
+        }
+    }
 }
 
 function recuperaArchivados(){
@@ -108,16 +122,17 @@ function recuperaArchivados(){
 
 function borraProducto(eliminable){
     
-    for( producto of col_productos.productos){
-        if(producto.nombre=eliminable){
-            posicion = col_productos.productos.indexOf(producto);
-            col_productos.productos.splice(posicion,1);
-        }
+    encontrado = col_productos.productos.filter(producto => producto.nombre == eliminable);
+    
+    if(encontrado.length>0){
+        posicion = col_productos.productos.indexOf(encontrado[0]);       
+        col_productos.productos.splice(posicion,1);
+    
+        localStorage.setItem( "col_productos", JSON.stringify(col_productos) );
+    
+        rellenaLista();
     }
-
-    localStorage.setItem( "col_productos", JSON.stringify(col_productos) );
-
-    rellenaLista();
+   
 
     //¿Y si el nombre está vacio??? :(
 }
